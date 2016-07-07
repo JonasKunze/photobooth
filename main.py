3884ad94b92e6a0b5407ef8a9d6b24a18478ee79
Jonas Kunze <kunze.jonas@gmail.com>
2016-07-07 13:58:07 +0200

    Using gpiozero because GPIO still creates some segfaults

--- main.py
+++ main.py
@@ -3,27 +3,24 @@
 from Display import Display 
 from Cam import Cam 
 from PIL import Image
-from gpio import *
+from gpiozero import LED, Button
+
 import time
 print("Starting!!")
 
 resolution = (1680, 1050)
 
 BUTTON_PIN = 3
-GPIO_init()
-button = Button(BUTTON_PIN, 100)
+button = Button(BUTTON_PIN)
 
-#cam = Cam()
+cam = Cam()
 with Display(resolution) as display:
-
-    # Load the arbitrarily sized image
-    #img = Image.open('overlay.png')
 
     display.show_video_fullscreen()
 
     def on_buzzer_pushed():
         print("buzzer pushed")
-        #filename = cam.take_pic()
+        filename = cam.take_pic()
         filename = "capt0000.jpg"
         print("showing image %s" % filename)
         with Image.open(filename) as img:
@@ -31,12 +28,9 @@ with Display(resolution) as display:
             display.show_image_fullscreen(img)
             cam.check_brightness()
 
-
-
-    button.on_pushed = on_buzzer_pushed
+    button.when_pressed = on_buzzer_pushed
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt, SystemExit:
         print("Closing app")
-        GPIO_cleanup()
